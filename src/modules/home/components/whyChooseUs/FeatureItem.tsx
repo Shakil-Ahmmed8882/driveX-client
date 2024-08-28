ff6ad783f4a1bbd245be3cb4113f379a92e147a5
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2024-08-28 15:35:02 +0600

    feat(Why Choose Us):Add cotents in card view

--- src/modules/home/components/whyChooseUs/FeatureItem.tsx
+++ src/modules/home/components/whyChooseUs/FeatureItem.tsx
@@ -13,6 +13,8 @@ type FeatureItemProps = {
 const FeatureItem: React.FC<FeatureItemProps> = ({ feature }) => {
   return (
     <div className="p-6 bg-[#252525db] shadow-md rounded-lg  ">
+
+        
       <div className="text-4xl mb-4 text-primaryColor">{feature.icon}</div>
       <h3 className="text-xl font-semibold mb-2">{feature.title}</h3>
       <p className="text-[#aaa9a9]">{feature.description}</p>
